6f698196f15216af05ae15cf0aafec1691b439c9
BaeDongWoo <ehddn5410@naver.com>
2024-05-01 01:20:33 +0900

    feat:hasPrice 및 버튼컬러, 사이즈 추가

--- app/components/common/Button/index.tsx
+++ app/components/common/Button/index.tsx
@@ -3,45 +3,59 @@ import { ButtonHTMLAttributes } from 'react';
 import styled from 'styled-components';
 import theme from '../../../styles/theme';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: 'small' | 'medium' | 'large' | 'full';
-  buttonTheme?: 'dark' | 'light';
+  size?: 'small' | 'medium' | 'large' | 'xlarge' | 'full';
+  buttonColor?: 'dark' | 'light' | 'selling' | 'buying';
   onClick?: () => void;
   className?: string;
+  hasPrice?: string;
 }
 const Button = ({
   type = 'button',
   size = 'full',
-  buttonTheme = 'dark',
+  buttonColor = 'dark',
+  hasPrice,
   onClick,
   disabled,
   children,
 }: ButtonProps) => {
-  const sizeValue = theme.button.common.size[size];
+  const sizeValue = theme.button.size[size];
+  const buttonColorValue = theme.button.color[buttonColor];
+  const fontSize = hasPrice ? '0.8rem' : theme.fontSize.caption3;
   return (
     <Container
       type={type}
-      buttonTheme={buttonTheme}
+      buttonColor={buttonColorValue}
       onClick={onClick}
       size={sizeValue}
       disabled={disabled}
+      fontSize={fontSize}
     >
       {children}
+      <div className="price">{hasPrice}</div>
     </Container>
   );
 };
-const Container = styled.button<{ buttonTheme: string; size: string }>`
+const Container = styled.button<{
+  buttonColor: string;
+  size: string;
+  fontSize: string;
+}>`
   height: 48px;
   border-radius: 10px;
   border: solid 1px ${theme.colors.border};
-  font-weight: bold;
+  font-size: ${(props) => props.fontSize};
   width: ${(props) => props.size};
-  color: ${(props) => (props.buttonTheme === 'dark' ? '#ffffff' : '#000000')};
-  background-color: ${(props) =>
-    props.buttonTheme === 'dark' ? '#000000' : '#ffffff'};
+  color: ${(props) =>
+    props.buttonColor === '#ffffff' ? '#000000' : '#ffffff'};
+  background-color: ${(props) => props.buttonColor};
   cursor: pointer;
   &:active {
     background-color: ${(props) =>
-      props.buttonTheme === 'dark' ? '#333333' : '#cccccc'};
+      props.buttonColor === '#000000' ? '#333333' : ''};
+  }
+  .price {
+    font-size: ${theme.fontSize.caption1};
+    margin-top: 3px;
   }
 `;
 export default Button;
