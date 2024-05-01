850f69261da52d8b2cb416c251afee39a0ff7057
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-01 16:37:13 +0900

    feat: Footer UI 구현

---
+++ app/components/common/Footer/index.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import theme from '@/styles/theme';
+import styled from 'styled-components';
+import { FaInstagram } from 'react-icons/fa';
+import { MdOutlineFacebook } from 'react-icons/md';
+import KakaoChannel from 'public/kakao-channel.svg';
+import Image from 'next/image';
+
+const Footer = () => {
+	return (
+		<StyledFooter>
+			<div>
+				<Flex>
+					<Row>
+						<li>
+							<a>서비스소개</a>
+						</li>
+						<li>
+							<a>제휴문의</a>
+						</li>
+						<li>
+							<a>FAQ</a>
+						</li>
+						<li>
+							<a>이용약관</a>
+						</li>
+						<li>
+							<strong>
+								<a>개인정보처리방침</a>
+							</strong>
+						</li>
+					</Row>
+					<Row>
+						<a>
+							<FaInstagram size={24} />
+						</a>
+						<a>
+							<MdOutlineFacebook size={24} />
+						</a>
+						<a>
+							<Image src='/kakao_channel.png' alt='kakao' width={27} height={27} />
+						</a>
+					</Row>
+				</Flex>
+				<Flex>
+					<Company>
+						<CompanyText>크림 주식회사 · 대표 김창욱</CompanyText>
+						<CompanyText>
+							사업자등록번호 : 570-88-01618 <a>사업자 정보 확인</a>
+						</CompanyText>
+						<CompanyText isLast>통신판매업 : 제 2021-성남분당C-0093호</CompanyText>
+						<CompanyText>
+							사업장소재지 : 경기도 성남시 분당구 분당내곡로 131 판교테크원 타워1, 8층
+						</CompanyText>
+						<CompanyText isLast>호스팅 서비스 : 네이버 클라우드 ㈜</CompanyText>
+					</Company>
+					<Copyright>© SHOESCREAM Corp.</Copyright>
+				</Flex>
+			</div>
+		</StyledFooter>
+	);
+};
+
+export default Footer;
+
+const StyledFooter = styled.footer`
+	padding: 4rem 4rem 3.5rem;
+	border-top: 0.1rem solid ${theme.colors.border};
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+`;
+
+const Flex = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding-bottom: 1.6rem;
+	position: relative;
+`;
+
+const Row = styled.ul`
+	display: flex;
+	gap: 2rem;
+	font-size: ${theme.fontSize.body1};
+	color: #000;
+
+	& a {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+	}
+`;
+
+const Company = styled.div`
+	max-width: 65.4rem;
+	color: ${theme.colors.text.secondary};
+	font-size: ${theme.fontSize.body2};
+	display: flex;
+	flex-wrap: wrap;
+	position: relative;
+
+	& a {
+		cursor: pointer;
+	}
+`;
+
+const CompanyText = styled.span<{ isLast?: boolean }>`
+	margin-right: ${(props) => (props.isLast ? 0 : '1.7rem')};
+	line-height: 2rem;
+`;
+
+const Copyright = styled.div`
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	font-size: ${theme.fontSize.caption2};
+	color: ${theme.colors.text.secondary};
+`;
