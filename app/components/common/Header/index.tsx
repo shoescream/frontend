576b8f0a936dbc891f963de91777e46404339032
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-05-02 01:11:10 +0900

    [#8] Feature: 공통 컴포넌트 구현(헤더) (#13)
    * feat:헤더 컴포넌트 기본 레이아웃
    
    * feat:navbar 기본 구조
    
    * feat:header 레이아웃
    
    * feat:search Bar 작업
    
    * refactor:navdata 분리
    
    * modify:props수정 event -> path
    
    * feat:active 기능 추가
    
    * feat:알람, 로그인 분리
    
    * feat:cursor pointer , 클릭 이벤트 추가
    
    * modify:yaml파일 수정
    
    * modify:px->rem 변경,console.log 삭제, nav Data path 수정

---
+++ app/components/common/Header/index.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+import MultiNavBar from './NavBar';
+import { NAV_DATA } from './navProps';
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <HeaderTop>
+        <MultiNavBar type="top" data={NAV_DATA.TOP_NAV_DATA} />
+      </HeaderTop>
+      <HeaderInner>
+        <div id="header__logo">KREAM</div>
+        <MultiNavBar type="main" data={NAV_DATA.MAIN_NAV_DATA} />
+        <SearchContainer>
+          <InputStyled type="text" placeholder="Search in site"></InputStyled>
+          <ImageStyled
+            src="/search.png"
+            alt="돋보기 이미지"
+            onClick={() => null}
+          ></ImageStyled>
+        </SearchContainer>
+      </HeaderInner>
+    </HeaderWrapper>
+  );
+};
+const HeaderWrapper = styled.header`
+  width: 144rem;
+  height: 18rem;
+  display: flex;
+  flex-direction: column;
+`;
+const HeaderTop = styled.div`
+  position: relative;
+  height: 10rem;
+`;
+const HeaderInner = styled.div`
+  display: flex;
+  padding: 0 5rem 0 5rem;
+  #header__logo {
+    font-size: ${theme.fontSize.headline1};
+    font-weight: bold;
+  }
+  position: relative;
+`;
+const InputStyled = styled.input`
+  height: 3.8rem;
+  width: 25rem;
+  border-radius: 0.6rem;
+  padding: 0.5rem;
+`;
+const ImageStyled = styled.img`
+  width: 3rem;
+  height: 3rem;
+  margin-left: 1rem;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  width: 30rem;
+  height: 4rem;
+  border: 0.1rem solid ${theme.colors.gray[200]};
+  border-radius: 0.6rem;
+  position: absolute;
+  right: 1rem;
+`;
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <main>hi</main>;
+  return <main>hi</main>;
 }
