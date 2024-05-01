555de44703353301d76c0bf76e1b78d53dea5974
BaeDongWoo <ehddn5410@naver.com>
2024-05-01 22:57:57 +0900

    modify:props수정 event -> path

--- app/components/common/Header/navProps.ts
+++ app/components/common/Header/navProps.ts
@@ -1,59 +1,33 @@
 const TOP_NAV_DATA = [
   {
     title: '고객센터',
-    event: () => {
-      console.log('고객센터');
-    },
+    path: 'center',
   },
   {
     title: '마이페이지',
-    event: () => {
-      console.log('마이페이지');
-    },
+    path: 'mypage',
   },
   {
     title: '관심상품',
-    event: () => {
-      console.log('관심상품');
-    },
-  },
-  {
-    title: '알림',
-    event: () => {
-      console.log('알림');
-    },
-  },
-  {
-    title: '로그인',
-    event: () => {
-      console.log('로그인');
-    },
+    path: 'favorites',
   },
 ];
 const MAIN_NAV_DATA = [
   {
     title: '홈',
-    event: () => {
-      console.log('홈');
-    },
+    path: '',
   },
   {
     title: '랭킹',
-    event: () => {
-      console.log('랭킹');
-    },
+    path: 'ranking',
   },
   {
     title: 'SHOP',
-    event: () => {
-      console.log('SHOP');
-    },
+    path: 'shop',
   },
   {
     title: '이벤트',
-    event: () => {
-      console.log('이벤트');
-    },
+    path: 'event',
   },
 ];
 
