ae1f3a6c37503144323de41c11330ca21e4f5c33
BaeDongWoo <ehddn5410@naver.com>
2024-05-01 21:59:21 +0900

    feat:search Bar 작업

--- app/components/common/Header/index.tsx
+++ app/components/common/Header/index.tsx
@@ -68,7 +68,10 @@ const Header = () => {
       <HeaderInner>
         <div id="header__logo">KREAM</div>
         <MultiNavBar type="main" data={mainDummy} />
-        <input type="text" id="search-bar"></input>
+        <SearchContainer>
+          <InputStyled type="text" placeholder="Search in site"></InputStyled>
+          <ImageStyled src="/search.png" alt="돋보기 이미지"></ImageStyled>
+        </SearchContainer>
       </HeaderInner>
     </HeaderWrapper>
   );
@@ -90,5 +93,25 @@ const HeaderInner = styled.div`
     font-size: ${theme.fontSize.headline1};
     font-weight: 1000;
   }
+  position: relative;
+`;
+const InputStyled = styled.input`
+  height: 38px;
+  width: 250px;
+  border-radius: 6px;
+  padding: 5px;
+`;
+const ImageStyled = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+`;
+const SearchContainer = styled.div`
+  width: 300px;
+  height: 40px;
+  border: 1px solid ${theme.colors.gray[200]};
+  border-radius: 6px;
+  position: absolute;
+  right: 10px;
 `;
 export default Header;
