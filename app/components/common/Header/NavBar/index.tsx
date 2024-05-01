0e671fea6deb3327732c7f132bedd924c05d6f48
BaeDongWoo <ehddn5410@naver.com>
2024-05-01 21:31:32 +0900

    feat:header 레이아웃

--- app/components/common/Header/NavBar/index.tsx
+++ app/components/common/Header/NavBar/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+import theme from '@/styles/theme';
 import styled from 'styled-components';
 
 interface NavDataProps {
@@ -8,12 +10,30 @@ interface NavProps {
   type?: 'top' | 'main';
   data?: NavDataProps[];
 }
-const MultiNavBar = ({ type = 'top' }: NavProps) => {
-  return <NavWrapper type={type}></NavWrapper>;
+const MultiNavBar = ({ type = 'top', data }: NavProps) => {
+  return (
+    <NavWrapper type={type}>
+      {data &&
+        data.map((navData, idx) => (
+          <ButtonWrapper onClick={navData.event} key={idx} type={type}>
+            {navData.title}
+          </ButtonWrapper>
+        ))}
+    </NavWrapper>
+  );
 };
-const NavWrapper = styled.div<{ type: string }>`
-    height : ${(props) => (props.type === 'top' ? '15px' : '40px')};
-    border : solid 1px black;
-    font-size
+const NavWrapper = styled.nav<{ type: string }>`
+  height: ${(props) => (props.type === 'top' ? '15px' : '42.5px')};
+  font-size: ${(props) =>
+    props.type === 'top' ? theme.fontSize.caption3 : theme.fontSize.title2};
+  position: ${(props) => (props.type === 'top' ? 'absolute' : 'static')};
+  top: 0;
+  right: 0;
+`;
+const ButtonWrapper = styled.button<{ type: string }>`
+  width: ${(props) => (props.type === 'top' ? '70px' : '100px')};
+  margin: 0 10px 0 5px;
+  background-color: #ffffff;
+  line-height: ${(props) => (props.type === 'top' ? '' : '42.5px')};
 `;
 export default MultiNavBar;
--- app/components/common/Header/index.tsx
+++ app/components/common/Header/index.tsx
@@ -1,13 +1,73 @@
-'use client';
 import styled from 'styled-components';
 import theme from '../../../styles/theme';
+import MultiNavBar from './NavBar';
 const Header = () => {
+  const dummy = [
+    {
+      title: '고객센터',
+      event: () => {
+        console.log('고객센터');
+      },
+    },
+    {
+      title: '마이페이지',
+      event: () => {
+        console.log('마이페이지');
+      },
+    },
+    {
+      title: '관심상품',
+      event: () => {
+        console.log('관심상품');
+      },
+    },
+    {
+      title: '알림',
+      event: () => {
+        console.log('알림');
+      },
+    },
+    {
+      title: '로그인',
+      event: () => {
+        console.log('로그인');
+      },
+    },
+  ];
+  const mainDummy = [
+    {
+      title: '홈',
+      event: () => {
+        console.log('홈');
+      },
+    },
+    {
+      title: '랭킹',
+      event: () => {
+        console.log('랭킹');
+      },
+    },
+    {
+      title: 'SHOP',
+      event: () => {
+        console.log('SHOP');
+      },
+    },
+    {
+      title: '이벤트',
+      event: () => {
+        console.log('이벤트');
+      },
+    },
+  ];
   return (
     <HeaderWrapper>
-      <nav id="top__nav">nav</nav>
+      <HeaderTop>
+        <MultiNavBar type="top" data={dummy} />
+      </HeaderTop>
       <HeaderInner>
         <div id="header__logo">KREAM</div>
-        <nav id="main__nav">nav</nav>
+        <MultiNavBar type="main" data={mainDummy} />
         <input type="text" id="search-bar"></input>
       </HeaderInner>
     </HeaderWrapper>
@@ -16,15 +76,19 @@ const Header = () => {
 const HeaderWrapper = styled.header`
   width: 1440px;
   height: 180px;
-  border: 1px solid black;
   display: flex;
   flex-direction: column;
 `;
+const HeaderTop = styled.div`
+  position: relative;
+  height: 100px;
+`;
 const HeaderInner = styled.div`
   display: flex;
+  padding: 0 50px 0 50px;
   #header__logo {
-    font-size: ${theme.fontSize.title2};
-    font-weight: bold;
+    font-size: ${theme.fontSize.headline1};
+    font-weight: 1000;
   }
 `;
 export default Header;
