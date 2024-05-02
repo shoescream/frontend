4a1bf3b0e5eadc782cb1342165059c698453b5e6
BaeDongWoo <ehddn5410@naver.com>
2024-05-02 22:39:51 +0900

    feat:layout 헤터, 푸터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,39 @@
 'use client';
 
 import { ReactQueryClientProvider } from './hooks/ReactQueryClientProvider';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 import theme from '@/styles/theme';
 import GlobalStyle from './GlobalStyle';
-
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 const inter = Inter({ subsets: ['latin'] });
 const pretendard = localFont({
-	src: '../public/fonts/PretendardVariable.woff2',
-	display: 'swap',
-	weight: '45 920',
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
 });
 
 interface RootLayoutProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-	return (
-		<ReactQueryClientProvider>
-			<ThemeProvider theme={theme}>
-				<html lang='en'>
-					<body className={`${inter.className} ${pretendard.className}`}>
-						<GlobalStyle />
-						<Container>
-							<header>header</header>
-							<Content>{children}</Content>
-							<footer>footer</footer>
-						</Container>
-					</body>
-				</html>
-			</ThemeProvider>
-		</ReactQueryClientProvider>
-	);
+  return (
+    <ReactQueryClientProvider>
+      <ThemeProvider theme={theme}>
+        <html lang="en">
+          <body className={`${inter.className} ${pretendard.className}`}>
+            <GlobalStyle />
+            <Header />
+            {children}
+            <Footer />
+          </body>
+        </html>
+      </ThemeProvider>
+    </ReactQueryClientProvider>
+  );
 };
 
 export default RootLayout;
-
-const Container = styled.div`
-	width: 100%;
-`;
-
-const Content = styled.div`
-	max-width: 128rem;
-	margin: 0 auto;
-	padding: 0 4rem;
-`;
