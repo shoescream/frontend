606d88e2a027151b296945240acdc85c61d8bac5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-05 19:33:56 +0900

    [#21] Feature: 메인 페이지 구현 (#22)
    * fix: 헤더 수정
    
    * style: 스타일 포맷 추가
    
    * feat: 메인페이지 구현
    
    * fix: 양 옆 패딩 삭제

--- app/components/common/Footer/index.tsx
+++ app/components/common/Footer/index.tsx
@@ -7,123 +7,120 @@ import { MdOutlineFacebook } from 'react-icons/md';
 import Image from 'next/image';
 
 const Footer = () => {
-    return (
-        <StyledFooter>
-            <div>
-                <Flex>
-                    <Row>
-                        {[
-                            '서비스소개',
-                            '제휴문의',
-                            'FAQ',
-                            '이용약관',
-                            '개인정보처리방침',
-                        ].map((item, idx) => (
-                            <li key={item}>
-                                {idx === 4 ? (
-                                    <a>
-                                        <strong>{item}</strong>
-                                    </a>
-                                ) : (
-                                    <a>{item}</a>
-                                )}
-                            </li>
-                        ))}
-                    </Row>
-                    <Row>
-                        <a>
-                            <FaInstagram size={24} />
-                        </a>
-                        <a>
-                            <MdOutlineFacebook size={24} />
-                        </a>
-                        <a>
-                            <Image
-                                src="/kakao_channel.png"
-                                alt="kakao"
-                                width={27}
-                                height={27}
-                            />
-                        </a>
-                    </Row>
-                </Flex>
-                <Flex>
-                    <Company>
-                        <CompanyText>크림 주식회사 · 대표 김창욱</CompanyText>
-                        <CompanyText>
-                            사업자등록번호 : 570-88-01618{' '}
-                            <a>사업자 정보 확인</a>
-                        </CompanyText>
-                        <CompanyText isLast>
-                            통신판매업 : 제 2021-성남분당C-0093호
-                        </CompanyText>
-                        <CompanyText>
-                            사업장소재지 : 경기도 성남시 분당구 분당내곡로 131
-                            판교테크원 타워1, 8층
-                        </CompanyText>
-                        <CompanyText isLast>
-                            호스팅 서비스 : 네이버 클라우드 ㈜
-                        </CompanyText>
-                    </Company>
-                    <Copyright>© SHOESCREAM Corp.</Copyright>
-                </Flex>
-            </div>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter>
+      <div>
+        <Flex>
+          <Row>
+            {[
+              '서비스소개',
+              '제휴문의',
+              'FAQ',
+              '이용약관',
+              '개인정보처리방침',
+            ].map((item, idx) => (
+              <li key={item}>
+                {idx === 4 ? (
+                  <a>
+                    <strong>{item}</strong>
+                  </a>
+                ) : (
+                  <a>{item}</a>
+                )}
+              </li>
+            ))}
+          </Row>
+          <Row>
+            <a>
+              <FaInstagram size={24} />
+            </a>
+            <a>
+              <MdOutlineFacebook size={24} />
+            </a>
+            <a>
+              <Image
+                src="/kakao_channel.png"
+                alt="kakao"
+                width={27}
+                height={27}
+              />
+            </a>
+          </Row>
+        </Flex>
+        <Flex>
+          <Company>
+            <CompanyText>크림 주식회사 · 대표 김창욱</CompanyText>
+            <CompanyText>
+              사업자등록번호 : 570-88-01618 <a>사업자 정보 확인</a>
+            </CompanyText>
+            <CompanyText isLast>
+              통신판매업 : 제 2021-성남분당C-0093호
+            </CompanyText>
+            <CompanyText>
+              사업장소재지 : 경기도 성남시 분당구 분당내곡로 131 판교테크원
+              타워1, 8층
+            </CompanyText>
+            <CompanyText isLast>호스팅 서비스 : 네이버 클라우드 ㈜</CompanyText>
+          </Company>
+          <Copyright>© SHOESCREAM Corp.</Copyright>
+        </Flex>
+      </div>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
 
 const StyledFooter = styled.footer`
-    padding: 4rem 4rem 3.5rem;
-    border-top: 0.1rem solid ${theme.colors.border};
-    width: 100%;
+  padding: 4rem 0rem 3.5rem;
+  border-top: 0.1rem solid ${theme.colors.border};
+  width: 100%;
 `;
 
 const Flex = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 1.6rem;
-    position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 1.6rem;
+  position: relative;
 `;
 
 const Row = styled.ul`
-    display: flex;
-    gap: 2rem;
-    font-size: ${theme.fontSize.body1};
-    color: #000;
+  display: flex;
+  gap: 2rem;
+  font-size: ${theme.fontSize.body1};
+  color: #000;
 
-    & a {
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-    }
+  & a {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
 `;
 
 const Company = styled.div`
-    max-width: 65.4rem;
-    color: ${theme.colors.text.secondary};
-    font-size: ${theme.fontSize.body2};
-    display: flex;
-    flex-wrap: wrap;
-    position: relative;
+  max-width: 65.4rem;
+  color: ${theme.colors.text.secondary};
+  font-size: ${theme.fontSize.body2};
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
 
-    & a {
-        cursor: pointer;
-    }
+  & a {
+    cursor: pointer;
+  }
 `;
 
 const CompanyText = styled.span<{ isLast?: boolean }>`
-    margin-right: ${(props) => (props.isLast ? 0 : '1.7rem')};
-    line-height: 2rem;
+  margin-right: ${(props) => (props.isLast ? 0 : '1.7rem')};
+  line-height: 2rem;
 `;
 
 const Copyright = styled.div`
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    font-size: ${theme.fontSize.caption2};
-    color: ${theme.colors.text.secondary};
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  font-size: ${theme.fontSize.caption2};
+  color: ${theme.colors.text.secondary};
 `;
--- app/components/common/Header/index.tsx
+++ app/components/common/Header/index.tsx
@@ -5,43 +5,44 @@ import { NAV_DATA } from './navProps';
 import { useRouter } from 'next/navigation';
 
 const Header = () => {
-	const router = useRouter();
-	return (
-		<HeaderWrapper>
-			<HeaderTop>
-				<MultiNavBar type='top' data={NAV_DATA.TOP_NAV_DATA} />
-			</HeaderTop>
-			<HeaderInner>
-				<div id='header__logo' onClick={() => router.push('/')}>
-					KREAM
-				</div>
-				<MultiNavBar type='main' data={NAV_DATA.MAIN_NAV_DATA} />
-				<SearchContainer>
-					<InputStyled type='text' placeholder='Search in site'></InputStyled>
-					<ImageStyled
-						src='/search.png'
-						alt='돋보기 이미지'
-						onClick={() => null}
-					></ImageStyled>
-				</SearchContainer>
-			</HeaderInner>
-		</HeaderWrapper>
-	);
+  const router = useRouter();
+  return (
+    <HeaderWrapper>
+      <HeaderTop>
+        <MultiNavBar type="top" data={NAV_DATA.TOP_NAV_DATA} />
+      </HeaderTop>
+      <HeaderInner>
+        <div id="header__logo" onClick={() => router.push('/')}>
+          KREAM
+        </div>
+        <MultiNavBar type="main" data={NAV_DATA.MAIN_NAV_DATA} />
+        <SearchContainer>
+          <InputStyled type="text" placeholder="Search in site"></InputStyled>
+          <ImageStyled
+            src="/search.png"
+            alt="돋보기 이미지"
+            onClick={() => null}
+          ></ImageStyled>
+        </SearchContainer>
+      </HeaderInner>
+    </HeaderWrapper>
+  );
 };
 
 const HeaderWrapper = styled.header`
-  width: 128rem;
+  width: 100%;
   height: 13rem;
   display: flex;
   flex-direction: column;
   border-bottom: 0.1rem solid ${theme.colors.border};
   margin: auto;
+  padding-bottom: 1rem;
 `;
 
 const HeaderTop = styled.div`
-	position: relative;
-	height: 10rem;
-	margin-top: 1rem;
+  position: relative;
+  height: 10rem;
+  margin-top: 1rem;
 `;
 
 const HeaderInner = styled.div`
@@ -56,25 +57,25 @@ const HeaderInner = styled.div`
 `;
 
 const InputStyled = styled.input`
-	height: 3.8rem;
-	width: 25rem;
-	border-radius: 0.6rem;
-	padding: 0.5rem;
+  height: 3.8rem;
+  width: 25rem;
+  border-radius: 0.6rem;
+  padding: 0.5rem;
 `;
 
 const ImageStyled = styled.img`
-	width: 3rem;
-	height: 3rem;
-	margin-left: 1rem;
-	cursor: pointer;
+  width: 3rem;
+  height: 3rem;
+  margin-left: 1rem;
+  cursor: pointer;
 `;
 const SearchContainer = styled.div`
-	width: 30rem;
-	height: 4rem;
-	border: 0.1rem solid ${theme.colors.gray[200]};
-	border-radius: 0.6rem;
-	position: absolute;
-	right: 1rem;
+  width: 30rem;
+  height: 4rem;
+  border: 0.1rem solid ${theme.colors.gray[200]};
+  border-radius: 0.6rem;
+  position: absolute;
+  right: 1rem;
 `;
 
 export default Header;
