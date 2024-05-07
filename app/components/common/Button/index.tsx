9948a9200cd44d0b16f392eacd2594a73866f597
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-07 12:39:30 +0900

    fix: 버튼 컴포넌트 수정

--- app/components/common/Button/index.tsx
+++ app/components/common/Button/index.tsx
@@ -1,75 +1,78 @@
-'use client';
-
-import { ButtonHTMLAttributes } from 'react';
-import styled, { CSSProperties } from 'styled-components';
+import { ButtonHTMLAttributes, CSSProperties } from 'react';
+import styled from 'styled-components';
 import theme from '../../../styles/theme';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	size?: 'small' | 'medium' | 'large' | 'xlarge' | 'full';
-	buttonColor?: 'dark' | 'light' | 'selling' | 'buying' | 'none';
-	onClick?: () => void;
-	className?: string;
-	hasPrice?: string;
-	styles?: CSSProperties;
+  size?: 'small' | 'medium' | 'large' | 'xlarge' | 'full';
+  buttonColor?: 'dark' | 'light' | 'selling' | 'buying' | 'none';
+  onClick?: () => void;
+  className?: string;
+  hasPrice?: string;
+  styles?: CSSProperties;
 }
 
 const Button = ({
-	type = 'button',
-	size = 'full',
-	buttonColor = 'dark',
-	hasPrice,
-	onClick,
-	disabled,
-	children,
-	styles,
+  type = 'button',
+  size = 'full',
+  buttonColor = 'dark',
+  hasPrice,
+  onClick,
+  disabled = false,
+  children,
+  styles,
 }: ButtonProps) => {
-	const sizeValue = theme.button.size[size];
-	const buttonColorValue = disabled
-		? theme.colors.gray[200]
-		: buttonColor === 'none'
-		? 'transparent'
-		: theme.button.color[buttonColor];
-	const fontSize = hasPrice ? '0.8rem' : theme.fontSize.caption3;
-	return (
-		<Container
-			type={type}
-			buttonColor={buttonColorValue}
-			onClick={onClick}
-			size={sizeValue}
-			disabled={disabled!}
-			fontSize={fontSize}
-			style={styles}
-		>
-			{children}
-			<div className='price'>{hasPrice}</div>
-		</Container>
-	);
+  const sizeValue = theme.button.size[size];
+  const buttonColorValue = disabled
+    ? theme.colors.gray[200]
+    : buttonColor === 'none'
+    ? 'transparent'
+    : theme.button.color[buttonColor];
+  const fontSize = hasPrice ? '0.8rem' : theme.fontSize.caption3;
+
+  return (
+    <Container
+      type={type}
+      buttonColor={buttonColorValue}
+      onClick={onClick}
+      disabled={disabled}
+      fontSize={fontSize}
+      size={sizeValue}
+      style={{
+        ...styles,
+        width: sizeValue,
+        backgroundColor: buttonColorValue,
+      }}
+    >
+      {children}
+      {hasPrice && <div className="price">{hasPrice}</div>}
+    </Container>
+  );
 };
 
 const Container = styled.button<{
-	buttonColor: string;
-	size: string;
-	fontSize: string;
-	disabled: boolean;
+  buttonColor: string;
+  size: string;
+  fontSize: string;
+  disabled: boolean;
 }>`
-	height: 48px;
-	border-radius: 10px;
-	border: solid 1px ${theme.colors.border};
-	font-size: ${(props) => props.fontSize};
-	width: ${(props) => props.size};
-	color: ${(props) => (props.buttonColor === '#ffffff' ? '#000000' : '#ffffff')};
-	background-color: ${(props) => props.buttonColor};
+  height: 48px;
+  border-radius: 10px;
+  border: solid 1px ${theme.colors.border};
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) =>
+    props.buttonColor === '#ffffff' ? '#000000' : '#ffffff'};
+  background-color: ${(props) => props.buttonColor};
   font-weight: bold;
   margin-top: 1rem;
-	cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
-	&:active {
-		background-color: ${(props) =>
-			props.buttonColor === '#000000' ? '#333333' : ''};
-	}
-	.price {
-		font-size: ${theme.fontSize.caption1};
-		margin-top: 3px;
-	}
+  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
+  &:active {
+    background-color: ${(props) =>
+      props.buttonColor === '#000000' ? '#333333' : ''};
+  }
+  .price {
+    font-size: ${theme.fontSize.caption1};
+    margin-top: 3px;
+  }
 `;
 
 export default Button;
