5f7bbfc59fc10bdc2b9ee61fb08e2c14c84fdf65
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2024-05-09 21:42:13 +0900

    [#32]Feature : 회원 가입 페이지 api연동 (#33)
    * feat:아이디 , 비밀번호 찾기 기본 레이아웃
    
    * feat:커스텀 toast 컴포넌트 추가
    
    * feat:이메일 인증 코드API
    
    * feat:정규식 수정 , 인정번호 폼 추가 , api호출 로직
    
    * feat:이메일 인증 번호 체크 api hooks
    
    * feat:이메일 인증번호 전송 api hook
    
    * feat:회원가입 요청 api
    
    * feat:api 호출 로직 추가, toast추가
    
    * feat:타이머 추가
    
    * feat:readonly 속성 추가
    
    * feat:아이디, 비밀번호 찾기 api hook
    
    * modify:버튼 스타일 및 라우트 주소 수정
    
    * feat:아이디,비밀번호 찾기 페이지 병합 및 api연결
    
    * modify:비밀번호 정규식 수정
    
    * fix:timer 버그 수정

--- app/(route)/join/page.tsx
+++ app/(route)/join/page.tsx
@@ -2,145 +2,256 @@
 
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
+import Timer from '@/components/common/Timer';
+import CustomToast from '@/components/common/Toast';
+import useCheckAuthCode from '@/hooks/queries/useCheckAuthcode';
+import useMailNumber from '@/hooks/queries/useGetAuthcode';
+import useJoin from '@/hooks/queries/useJoin';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 interface FormData {
-    id: string;
-    password: string;
-    email: string;
-    username: string;
+  id: string;
+  password: string;
+  email: string;
+  username: string;
+  authNumber: number;
 }
 const Join = () => {
-    const {
-        register,
-        handleSubmit,
-        getValues,
-        formState: { errors },
-    } = useForm<FormData>({
-        mode: 'onChange',
+  const [mailSuccess, setMailSuccess] = useState(false);
+  const [isToast, setIsToast] = useState(false);
+  const [toastProps, setToastProps] = useState({ success: false, message: '' });
+  const [time, setTime] = useState(5);
+
+  const onToast = (success: boolean, message: string) => {
+    setIsToast(true);
+    setToastProps({ success: success, message: message });
+    setTimeout(function () {
+      setIsToast(false);
+    }, 2000);
+  };
+
+  const onClickAuthNumber = (isMail: boolean) => {
+    setMailSuccess(isMail);
+  };
+
+  const setTimer = (timer: number) => {
+    setTime(timer * 60 * 1000);
+  };
+
+  const mailNumber = useMailNumber({ onToast, onClickAuthNumber, setTimer });
+  const checkAuthCode = useCheckAuthCode({ onToast, onClickAuthNumber });
+  const join = useJoin();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: 'onChange',
+  });
+
+  const onSubmit = (data: FormData) => {
+    const { email, id, password, username } = data;
+    join.mutate({
+      memberId: id,
+      password: password,
+      name: username,
+      email: email,
     });
-    const onSubmit = (data: FormData) => {
-        //data submit
-    };
-    const checkEmail = () => {
-        const email = getValues('email');
-        //email 체크
-    };
-    const router = useRouter();
-    return (
-        <JoinContainer>
-            <Title>회원가입</Title>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input
-                    type="text"
-                    label="아이디*"
-                    name="id"
-                    placeholder="아이디를 입력해주세요"
-                    errormessage={errors.id?.message || ''}
-                    register={register}
-                    rules={{
-                        pattern: {
-                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                            message: '올바르지 않은 형식입니다.',
-                        },
-                    }}
-                ></Input>
-                <Input
-                    type="password"
-                    label="비밀번호*"
-                    name="password"
-                    placeholder="비밀번호를 입력해주세요"
-                    errormessage={errors.password?.message || ''}
-                    register={register}
-                    rules={{
-                        pattern: {
-                            value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\\\|\[\]{};:'",.<>\/?]).{8,16}$/,
-                            message:
-                                '영문, 숫자, 특수문자를 조합해서 입력해주세요. (8~16글자)',
-                        },
-                        maxLength: {
-                            value: 16,
-                            message:
-                                '영문, 숫자, 특수문자를 조합해서 입력해주세요. (8~16글자)',
-                        },
-                        minLength: {
-                            value: 8,
-                            message:
-                                '영문, 숫자, 특수문자를 조합해서 입력해주세요. (8~16글자)',
-                        },
-                    }}
-                ></Input>
-                <EmailFormWrapper>
-                    <Input
-                        type="text"
-                        label="이메일*"
-                        name="email"
-                        placeholder="이메일을 입력해주세요"
-                        errormessage={errors.email?.message || ''}
-                        register={register}
-                        rules={{
-                            required: true,
-                            pattern: {
-                                value: /^\S+@\S+$/i,
-                                message: '이메일 형식에 알맞게 입력해주세요.',
-                            },
-                        }}
-                    ></Input>
-                    <Button
-                        size="small"
-                        onClick={checkEmail}
-                        styles={{
-                            height: '3.8rem',
-                            marginTop: '3.6rem',
-                            marginLeft: '1rem',
-                        }}
-                    >
-                        확인
-                    </Button>
-                </EmailFormWrapper>
-                <Input
-                    type="text"
-                    label="이름*"
-                    name="username"
-                    placeholder="성함을 입력해주세요"
-                    errormessage={errors.username?.message || ''}
-                    register={register}
-                    rules={{
-                        required: true,
-                        pattern: {
-                            value: /^[가-힣]+$/,
-                            message: '입력하신 성함이 올바르지 않습니다.',
-                        },
-                    }}
-                ></Input>
-                <Button type="submit" styles={{ marginTop: '2rem' }}>
-                    가입하기
-                </Button>
-                <Button buttonColor="light" onClick={() => router.push('/')}>
-                    돌아가기
-                </Button>
-            </form>
-        </JoinContainer>
-    );
+  };
+
+  const getAuthNumber = () => {
+    const email = getValues('email');
+    mailNumber.mutate({ mail: email });
+  };
+
+  const checkEmail = () => {
+    const authNumber = getValues('authNumber');
+    const email = getValues('email');
+    checkAuthCode.mutate({ mail: email, authNumber: authNumber });
+  };
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (time === 0) {
+      onToast(false, '시간이 만료 되었습니다.');
+      setTimer(5);
+      setMailSuccess(false);
+    }
+  });
+  return (
+    <JoinContainer>
+      <Title>회원가입</Title>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="text"
+          label="아이디*"
+          name="id"
+          placeholder="아이디를 입력해주세요"
+          errormessage={errors.id?.message || ''}
+          register={register}
+          rules={{
+            required: true,
+            pattern: {
+              value: /^[a-zA-Z]+([a-zA-Z0-9])*$/,
+              message: '올바르지 않은 형식입니다.',
+            },
+          }}
+        ></Input>
+        <Input
+          type="password"
+          label="비밀번호*"
+          name="password"
+          placeholder="비밀번호를 입력해주세요"
+          errormessage={errors.password?.message || ''}
+          register={register}
+          rules={{
+            required: {
+              value: true,
+              message: '필수 값입니다.',
+            },
+            pattern: {
+              value:
+                /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\\\|\[\]{};:'",.<>\/?]).{9,}$/,
+              message:
+                '영문, 숫자, 특수문자를 조합해서 9자 이상 입력해주세요. (대소문자 구분)',
+            },
+            minLength: {
+              value: 9,
+              message:
+                '영문, 숫자, 특수문자를 조합해서 9자 이상 입력해주세요. (대소문자 구분)',
+            },
+          }}
+        ></Input>
+        <EmailFormWrapper>
+          <Input
+            type="text"
+            label="이메일*"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+            errormessage={errors.email?.message || ''}
+            register={register}
+            rules={{
+              required: true,
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: '이메일 형식에 알맞게 입력해주세요.',
+              },
+            }}
+            styles={{ width: '30rem' }}
+          ></Input>
+          <Button
+            size="medium"
+            onClick={getAuthNumber}
+            styles={{
+              height: '3.8rem',
+              marginTop: '3.6rem',
+              marginLeft: '1rem',
+            }}
+          >
+            인증번호 받기
+          </Button>
+        </EmailFormWrapper>
+        <AuthNumberWrapper mailSuccess={mailSuccess ? 'true' : 'false'}>
+          <Input
+            type="text"
+            label="인증번호 입력*"
+            name="authNumber"
+            placeholder="이메일로 받은 인증번호를 입력해주세요"
+            errormessage={errors.authNumber?.message || ''}
+            register={register}
+            rules={{
+              required: true,
+              pattern: {
+                value: /^[0-9]+$/,
+                message: '숫자만 입력 가능합니다',
+              },
+            }}
+            readonly={!mailSuccess}
+            styles={{ width: '40rem' }}
+          ></Input>
+          <Button
+            size="small"
+            onClick={checkEmail}
+            styles={{
+              height: '3.8rem',
+              marginTop: '3.6rem',
+              marginLeft: '1rem',
+            }}
+            disabled={!mailSuccess}
+          >
+            확인
+          </Button>
+          {mailSuccess && <Timer time={time} setTime={setTime} />}
+        </AuthNumberWrapper>
+        <Input
+          type="text"
+          label="이름*"
+          name="username"
+          placeholder="성함을 입력해주세요"
+          errormessage={errors.username?.message || ''}
+          register={register}
+          rules={{
+            required: true,
+            pattern: {
+              value: /^[가-힣]+$/,
+              message: '입력하신 성함이 올바르지 않습니다.',
+            },
+          }}
+        ></Input>
+        <Button
+          type="submit"
+          styles={{
+            marginTop: '2rem',
+            fontSize: ' 1.6rem',
+            color: 'white',
+            fontWeight: '700',
+          }}
+        >
+          가입하기
+        </Button>
+        <Button
+          buttonColor="light"
+          onClick={() => router.push('/login')}
+          styles={{
+            fontSize: ' 1.6rem',
+            fontWeight: '700',
+          }}
+        >
+          돌아가기
+        </Button>
+      </form>
+      <CustomToast {...toastProps} isToast={isToast}></CustomToast>
+    </JoinContainer>
+  );
 };
 
 export default Join;
 
 const JoinContainer = styled.div`
-    height: 100%;
-    width: 40rem;
-    margin: 0 auto;
-    padding: 6rem 0 16rem;
+  height: 100%;
+  width: 40rem;
+  margin: 0 auto;
+  padding: 6rem 0 16rem;
 `;
+
 const Title = styled.h2`
-    text-align: center;
-    padding-bottom: 5rem;
+  text-align: center;
+  padding-bottom: 5rem;
 `;
+
 const EmailFormWrapper = styled.div`
-    display: flex;
-    width: 37rem;
-    margin: 0 1.5rem;
-    position: relative;
+  display: flex;
+  width: 40rem;
+  position: relative;
+`;
+
+const AuthNumberWrapper = styled.div<{ mailSuccess: string }>`
+  // display: ${(props) => (props.mailSuccess === 'true' ? 'flex' : 'none')};
+  display: flex;
+  width: 40rem;
+  position: relative;
 `;
--- app/components/common/Input/index.tsx
+++ app/components/common/Input/index.tsx
@@ -5,38 +5,41 @@ import styled, { CSSProperties } from 'styled-components';
 import { RegisterOptions, UseFormRegister } from 'react-hook-form';
 
 interface InputProps {
-	label?: string;
-	name: string;
-	register: UseFormRegister<any>;
-	rules?: RegisterOptions;
-	type?: string;
-	errormessage: string;
-	placeholder?: string;
-	styles?: CSSProperties;
+  label?: string;
+  name: string;
+  register: UseFormRegister<any>;
+  rules?: RegisterOptions;
+  type?: string;
+  errormessage: string;
+  placeholder?: string;
+  styles?: CSSProperties;
+  readonly?: boolean;
 }
 
 const Input = ({
-	label,
-	type,
-	name,
-	register,
-	rules,
-	errormessage,
-	placeholder,
-	styles,
+  label,
+  type,
+  name,
+  register,
+  rules,
+  errormessage,
+  placeholder,
+  styles,
+  readonly = false,
 }: InputProps) => {
-	return (
-		<InputWrapper style={styles}>
-			<Label errormessage={errormessage}>{label}</Label>
-			<StyledInput
-				type={type}
-				{...register(name, rules)}
-				errormessage={errormessage}
-				placeholder={placeholder}
-			/>
-			{errormessage && <ErrorText>{errormessage}</ErrorText>}
-		</InputWrapper>
-	);
+  return (
+    <InputWrapper style={styles}>
+      <Label errormessage={errormessage}>{label}</Label>
+      <StyledInput
+        type={type}
+        {...register(name, rules)}
+        errormessage={errormessage}
+        placeholder={placeholder}
+        readOnly={readonly}
+      />
+      {errormessage && <ErrorText>{errormessage}</ErrorText>}
+    </InputWrapper>
+  );
 };
 
 Input.displayName = 'Input';
@@ -44,50 +47,50 @@ Input.displayName = 'Input';
 export default Input;
 
 const InputWrapper = styled.div`
-	padding: 1rem 0 1.4rem;
-	position: relative;
-	height: 8rem;
-	margin: 0 auto;
+  padding: 1rem 0 1.4rem;
+  position: relative;
+  height: 8rem;
+  margin: 0 auto;
 `;
 
 const Label = styled.label<Pick<InputProps, 'errormessage'>>`
-    font-size: ${theme.fontSize.body2};
-    line-height: 1.8rem;
-    color: ${(props) => (props.errormessage ? '#f15746' : theme.colors.main)};
-    font-weight: 600;
+  font-size: ${theme.fontSize.body2};
+  line-height: 1.8rem;
+  color: ${(props) => (props.errormessage ? '#f15746' : theme.colors.main)};
+  font-weight: 600;
 `;
 
 const StyledInput = styled.input<Pick<InputProps, 'errormessage'>>`
-    border-bottom: 0.1rem solid
-        ${(props) => (props.errormessage ? '#f15746' : theme.colors.border)};
-    border-top: 0;
-    border-left: 0;
-    border-right: 0;
-    height: 3.8rem;
-    width: 100%;
-    outline: none;
-    font-size: ${theme.fontSize.subtitle3};
-    color: ${theme.colors.main};
-    padding-bottom: 1.4rem;
-    padding-top: 1.4rem;
-    margin-top: 0.4rem;
+  border-bottom: 0.1rem solid
+    ${(props) => (props.errormessage ? '#f15746' : theme.colors.border)};
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  height: 3.8rem;
+  width: 100%;
+  outline: none;
+  font-size: ${theme.fontSize.subtitle3};
+  color: ${theme.colors.main};
+  padding-bottom: 1.4rem;
+  padding-top: 1.4rem;
+  margin-top: 0.4rem;
+  &::placeholder {
+    color: #bcbcbc;
+  }
+  &:focus {
+    border-bottom: ${(props) =>
+      props.errormessage
+        ? '0.1rem solid #f15746'
+        : `0.2rem solid ${theme.colors.main}`};
     &::placeholder {
-        color: #bcbcbc;
+      color: white;
     }
-    &:focus {
-        border-bottom: ${(props) =>
-            props.errormessage
-                ? '0.1rem solid #f15746'
-                : `0.2rem solid ${theme.colors.main}`};
-        &::placeholder {
-            color: white;
-        }
-    }
+  }
 `;
 
 const ErrorText = styled.p`
-    color: #f15746;
-    font-size: ${theme.fontSize.caption2};
-    line-height: 1.6rem;
-    margin-top: 0.2rem;
+  color: #f15746;
+  font-size: ${theme.fontSize.caption2};
+  line-height: 1.6rem;
+  margin-top: 0.2rem;
 `;
