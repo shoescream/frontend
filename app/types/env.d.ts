dd52ccaceb9edf533cbe92b44c7f492a9a41f0b1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-09 15:50:05 +0900

    feat: env 타입 선언 추가

--- app/types/env.d.ts
+++ app/types/env.d.ts
@@ -1,5 +1,12 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_KAKAO_REST_API_KEY: string;
+    NEXT_PUBLIC_KAKAO_RETURN_URL: string;
+    NEXT_PUBLIC_KAKAO_CLIENT_SECRET: string;
   }
+}
+
+interface Window {
+  Kakao: any;
 }
