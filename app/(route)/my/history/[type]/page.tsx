b6a3f8eb02523d77ad0e9df641dca36efdcbe235
BaeDongWoo <ehddn5410@naver.com>
2024-05-17 20:04:31 +0900

    feat:날짜 선택 폼

--- app/(route)/my/history/[type]/page.tsx
+++ app/(route)/my/history/[type]/page.tsx
@@ -2,6 +2,11 @@
 import theme from '@/styles/theme';
 import { useState } from 'react';
 import styled from 'styled-components';
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@/components/common/Button';
 
 const data = {
   selling: [
@@ -18,12 +23,19 @@ const data = {
 
 const MyHistory = (props: any) => {
   const [selectState, setSelectState] = useState([1, 0, 0]);
-
+  const day = dayjs();
+  const [endDate, setEndDate] = useState(day);
+  const [startDate, setStartDate] = useState(day.add(-2, 'month'));
   const type = props.params.type;
 
   const title = type === 'selling' ? '구매' : '판매';
 
   const historyData = type === 'selling' ? data.selling : data.buying;
+
+  const dateHandler = (month: number) => {
+    setEndDate(day);
+    setStartDate(day.add(-month, 'month'));
+  };
 
   const stateHandler = (idx: number) => {
     let updateState = [0, 0, 0];
@@ -45,6 +57,81 @@ const MyHistory = (props: any) => {
           </State>
         ))}
       </StateWrapper>
+      <SelectDateWrapper>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateWrapper>
+            <Button
+              size="small"
+              buttonColor="light"
+              styles={{
+                height: '3rem',
+                marginTop: '1rem',
+                marginRight: '1rem',
+              }}
+              onClick={() => dateHandler(2)}
+            >
+              최근 2개월
+            </Button>
+            <Button
+              size="small"
+              buttonColor="light"
+              styles={{
+                height: '3rem',
+                marginTop: '1rem',
+                marginRight: '1rem',
+              }}
+              onClick={() => dateHandler(4)}
+            >
+              4개월
+            </Button>
+            <Button
+              size="small"
+              buttonColor="light"
+              styles={{
+                height: '3rem',
+                marginTop: '1rem',
+                marginRight: '1rem',
+              }}
+              onClick={() => dateHandler(6)}
+            >
+              6개월
+            </Button>
+            <DateItem>
+              <DatePicker
+                sx={{ backgroundColor: '#fff' }}
+                value={startDate}
+                className="date-picker"
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                  },
+                }}
+              />
+            </DateItem>
+            <DateItem>
+              <DatePicker
+                value={endDate}
+                className="date-picker"
+                slotProps={{
+                  textField: {
+                    size: 'small',
+                  },
+                }}
+              />
+            </DateItem>
+            <Button
+              size="small"
+              styles={{
+                height: '4rem',
+                marginTop: '0.55rem',
+                fontSize: theme.fontSize.caption1,
+              }}
+            >
+              조회
+            </Button>
+          </DateWrapper>
+        </LocalizationProvider>
+      </SelectDateWrapper>
     </>
   );
 };
@@ -60,6 +147,38 @@ const State = styled.div<{ select: number }>`
   width: 30rem;
   height: 5rem;
   text-align: center;
-  border-bottom: ${(props) => (props.select === 0 ? '0.1rem' : '0.5rem')} solid
-    ${theme.colors.gray[600]};
+  border-bottom: ${(props) => (props.select === 0 ? '0.1rem' : '0.3rem')} solid
+    black;
+  cursor: pointer;
+`;
+
+const SelectDateWrapper = styled.div`
+  width: 90rem;
+  height: 10rem;
+  background-color: ${theme.colors.gray[100]};
+  margin: auto;
+  padding-top: 3rem;
+`;
+
+const DateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DateItem = styled.div`
+  width: 15rem;
+  margin: 1rem;
+  line-height: 5rem;
+  * {
+    font-size: ${theme.fontSize.caption1}
+    font-weight: bold;
+  }
+  .date-picker{
+    background-color:#fff;
+    border-radius:0.4rem;
+    div{
+        top:0.25rem;
+       
+    }
+  }
 `;
