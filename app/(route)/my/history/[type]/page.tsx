feb56b75f4190be35ee480d2cd958ac73f026382
BaeDongWoo <ehddn5410@naver.com>
2024-05-21 13:31:04 +0900

    feat:아이템 옵션 및 상품 박스 레이아웃

--- app/(route)/my/history/[type]/page.tsx
+++ app/(route)/my/history/[type]/page.tsx
@@ -42,6 +42,57 @@ const MyHistory = (props: any) => {
     updateState[idx] = 1;
     setSelectState(updateState);
   };
+
+  const setTitle = () => {
+    if (selectState[0] === 1) {
+      return (
+        <>
+          <p>{title} 희망 가격</p>
+          <p>만료일</p>
+        </>
+      );
+    } else if (selectState[1] === 1) {
+      return (
+        <>
+          <p>상태</p>
+        </>
+      );
+    } else if (selectState[2] === 1) {
+      return (
+        <>
+          <p>{title} 일</p>
+          <p>상태</p>
+        </>
+      );
+    }
+  };
+
+  const setOption = () => {
+    if (selectState[0] === 1) {
+      return (
+        <>
+          <option>전체</option>
+          <option>만료 일</option>
+          <option>가격</option>
+        </>
+      );
+    } else if (selectState[1] === 1) {
+      return (
+        <>
+          <option>전체</option>
+          <option>상태</option>
+        </>
+      );
+    } else if (selectState[2] === 1) {
+      return (
+        <>
+          <option>전체</option>
+          <option>만료 일</option>
+          <option>가격</option>
+        </>
+      );
+    }
+  };
   return (
     <>
       <h2>{title}내역</h2>
@@ -51,8 +102,13 @@ const MyHistory = (props: any) => {
             key={idx}
             onClick={() => stateHandler(idx)}
             select={selectState[idx]}
+            type={type}
           >
-            <h3>{data.count}</h3>
+            <h3
+              id={'history_count' + (selectState[idx] === 1 ? '_select' : '')}
+            >
+              {data.count}
+            </h3>
             <h3>{title + ' ' + data.title}</h3>
           </State>
         ))}
@@ -132,6 +188,20 @@ const MyHistory = (props: any) => {
           </DateWrapper>
         </LocalizationProvider>
       </SelectDateWrapper>
+      <StateOptionBox>
+        <SortOption>{setOption()}</SortOption>
+        <OptionTitle>{setTitle()}</OptionTitle>
+      </StateOptionBox>
+      <ItemBox>
+        <ProductInfo>
+          <img />
+          <ProductNameOption>
+            <p id="product_name">상품 명</p>
+            <p id="product_option">상품 옵션</p>
+          </ProductNameOption>
+        </ProductInfo>
+        <ItemOption>{setTitle()}</ItemOption>
+      </ItemBox>
     </>
   );
 };
@@ -143,13 +213,25 @@ const StateWrapper = styled.div`
   justify-content: center;
 `;
 
-const State = styled.div<{ select: number }>`
+const State = styled.div<{ select: number; type: string }>`
   width: 30rem;
   height: 5rem;
   text-align: center;
   border-bottom: ${(props) => (props.select === 0 ? '0.1rem' : '0.3rem')} solid
     black;
   cursor: pointer;
+
+  #history_count_select {
+    color: ${(props) =>
+      props.type === 'selling' ? theme.colors.buying : theme.colors.selling};
+  }
+  #history_count {
+    color: black;
+  }
+  h3 {
+    color: ${(props) =>
+      props.select === 0 ? theme.colors.gray[200] : 'black'};
+  }
 `;
 
 const SelectDateWrapper = styled.div`
@@ -180,5 +262,70 @@ const DateItem = styled.div`
         top:0.25rem;
        
     }
+  }
+`;
+
+const StateOptionBox = styled.div`
+  width: 90rem;
+  margin: 1rem auto;
+  padding: 1rem 1rem 1rem 0;
+  border-bottom: 0.1rem solid ${theme.colors.gray[200]};
+  display: flex;
+  justify-content: space-between;
+`;
+
+const OptionTitle = styled.div`
+  display: flex;
+  p {
+    margin: 0 5rem;
+    padding-top: 2rem;
+  }
+`;
+
+const SortOption = styled.select`
+  padding: 1rem;
+  border: 0.1rem solid ${theme.colors.gray[200]};
+  border-radius: 1rem;
+  width: 12rem;
+  height: 4rem;
+  cursor: pointer;
+`;
+
+const ItemBox = styled.div`
+  width: 90rem;
+  margin: 1rem auto;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  border: 0.1rem solid ${theme.colors.gray[200]};
+  border-radius: 1rem;
+  img {
+    width: 8rem;
+    height: 8rem;
+    float: left;
+    margin-right: 1rem;
+  }
+`;
+
+const ProductInfo = styled.div`
+  display: flex;
+`;
+
+const ItemOption = styled.div`
+  display: flex;
+  line-height: 8rem;
+  p {
+    margin: 0 5rem;
+  }
+`;
+
+const ProductNameOption = styled.div`
+  #product_name {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+  #product_option {
+    font-size: ${theme.fontSize.subtitle3};
+    color: ${theme.colors.gray[200]};
   }
 `;
