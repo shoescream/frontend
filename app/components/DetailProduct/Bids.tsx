18a4c6dc69ea4833991b9bd7399983a172834f6c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-05-28 18:33:33 +0900

    fix: 체결 거래 및 입찰 내역 없을 경우 컴포넌트 숨김 처리

--- app/components/DetailProduct/Bids.tsx
+++ app/components/DetailProduct/Bids.tsx
@@ -26,30 +26,34 @@ const Bids = ({
   });
 
   return (
-    <div style={{ marginTop: '2rem' }}>
-      <FilterBox
-        data={['체결 거래', '판매 입찰', '구매 입찰']}
-        onClick={(item) => onSetCurrentFilterBySize(item)}
-        currentClickedItem={currentFilterBySize}
-      >
-        <BuyingTable
-          data={
-            currentFilterBySize === '판매 입찰'
-              ? bidData?.sellingBidResponse!
-              : currentFilterBySize === '구매 입찰'
-              ? bidData?.buyingBidResponse!
-              : transactionData?.dealResponse!.map((el) => {
-                  return {
-                    createdAt: el.tradedAt,
-                    size: el.size,
-                    price: el.price,
-                    quantity: NaN,
-                  };
-                })!
-          }
-        />
-      </FilterBox>
-    </div>
+    <>
+      {bidData && transactionData && (
+        <div style={{ marginTop: '2rem' }}>
+          <FilterBox
+            data={['체결 거래', '판매 입찰', '구매 입찰']}
+            onClick={(item) => onSetCurrentFilterBySize(item)}
+            currentClickedItem={currentFilterBySize}
+          >
+            <BuyingTable
+              data={
+                currentFilterBySize === '판매 입찰'
+                  ? bidData?.sellingBidResponse!
+                  : currentFilterBySize === '구매 입찰'
+                  ? bidData?.buyingBidResponse!
+                  : transactionData?.dealResponse!.map((el) => {
+                      return {
+                        createdAt: el.tradedAt,
+                        size: el.size,
+                        price: el.price,
+                        quantity: NaN,
+                      };
+                    })!
+              }
+            />
+          </FilterBox>
+        </div>
+      )}
+    </>
   );
 };
 
