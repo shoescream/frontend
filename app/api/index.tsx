bab3b2a27050554d997a9904c263484df90f0ab4
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-06-03 12:12:57 +0900

    fix: instance 수정

--- app/api/index.tsx
+++ app/api/index.tsx
@@ -1,5 +1,5 @@
-import LocalStorage from '@/utils/localStorage';
-import axios, { InternalAxiosRequestConfig } from 'axios';
+// import LocalStorage from '@/utils/localStorage';
+import axios from 'axios';
 
 const Instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -9,36 +9,37 @@ const Instance = axios.create({
   withCredentials: true,
 });
 
-Instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  config.headers.authorization = LocalStorage.getItem('@token');
-  return config;
-});
+// TODO: 추후 토큰 관리 시 적용
+// Instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+//   config.headers.authorization = LocalStorage.getItem('@token');
+//   return config;
+// });
 
-Instance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const status = error.response?.status;
-    const originalRequest = error.config;
+// Instance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const status = error.response?.status;
+//     const originalRequest = error.config;
 
-    if (status === 401) {
-      const token = localStorage.getItem('@token');
-      const refresh = localStorage.getItem('@refresh');
+//     if (status === 401) {
+//       const token = localStorage.getItem('@token');
+//       const refresh = localStorage.getItem('@refresh');
 
-      // const refreshTokenResponse = await fetchRefresh()
-      // const newToken = refreshTokenResponse.data.accessToken;
-      // localStorage.setItem('@token', newToken)
+//       // const refreshTokenResponse = await fetchRefresh()
+//       // const newToken = refreshTokenResponse.data.accessToken;
+//       // localStorage.setItem('@token', newToken)
 
-      // originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-      return Instance(originalRequest);
-    } else if (status === 400) {
-      return Promise.reject('존재하지 않는 데이터입니다.');
-    } else if (status === 404) {
-      return Promise.reject('잘못된 접근입니다.');
-    } else if (status === 500) {
-      return Promise.reject('서버 에러');
-    }
-    return Promise.reject(error);
-  }
-);
+//       // originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+//       return Instance(originalRequest);
+//     } else if (status === 400) {
+//       return Promise.reject('존재하지 않는 데이터입니다.');
+//     } else if (status === 404) {
+//       return Promise.reject('잘못된 접근입니다.');
+//     } else if (status === 500) {
+//       return Promise.reject('서버 에러');
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export { Instance };
